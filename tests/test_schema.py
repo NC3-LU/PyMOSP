c0126465d0a3496540b07e012c4dceb792a15290
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-03-26 18:23:20 +0100

    new: [tests] added files for the future tests on JSON schemas.

---
+++ tests/test_schema.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import os
+import unittest
+
+import pymosp
+
+
+class TestSchema(unittest.TestCase):
+    def setUp(self):
+        token = os.getenv("MOSP_TOKEN")
+        self.mosp = pymosp.PyMOSP("https://objects.monarc.lu/api/v2/", token)
+
+    def test_get_all_schemas(self):
+        r = self.mosp.schemas()
+        assert r.json()["metadata"]["count"] != '0', "no result"
